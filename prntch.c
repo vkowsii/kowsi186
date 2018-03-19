c77ca7e8f1497b408c388f7159e98c5afe7c2cb2
vkowsii <30829409+vkowsii@users.noreply.github.com>
2018-03-19 16:14:23 +0530

    Create prntch.c

---
+++ prntch.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char a[50];
+    int n;
+    printf("enter the value");
+    scanf("%s",&a);
+    printf("enter the value");
+    scanf("%d",&n);
+    for(i=0;i<2;i++)
+    printf("%d",a[i]);
+}
